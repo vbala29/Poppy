20fdc2eb5b450e12deb596b5850d3cefcf685dca
Vikram Bala <vikrambala2002@gmail.com>
2024-08-27 02:32:10 -0400

    vercel config for cron and cors

--- src/python/dailyUpdater/dailyUpdater.py
+++ src/python/dailyUpdater/dailyUpdater.py
@@ -1,11 +1,11 @@
 import requests
 
 def update_daily_info():
-    url = 'http://localhost:3000/api/cron'
+    url = 'https://poppy-fawn.vercel.app/api/cron'
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            print('Daily cron ran successfully', response.json())
+            print('Daily cron ran successfully. Response:', response.json())
         else:
             print('Daily cron failed with code:', response.status_code)
     except Exception as e:
