143d4e65a51ca805cff08c101bbf997d240b08db
Vikram Bala <vikrambala2002@gmail.com>
2024-08-27 16:33:32 -0400

    viewport scale for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1"
         ></meta>
       </Head>
 
