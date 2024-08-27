80a88468d3315444b68ed2298d91121ab549dafd
Vikram Bala <vikrambala2002@gmail.com>
2024-08-27 16:30:32 -0400

    Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PopQuiz",
+  title: "Poppy",
   description: "A minigame by Vikram Bala",
 };
 
